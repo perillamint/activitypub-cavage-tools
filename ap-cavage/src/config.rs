efe569f5ae34df10398737ff2537793b467c769a
perillamint <perillamint@silicon.moe>
2023-07-02 16:19:31 +0900

    Initial working prototype

---
+++ ap-cavage/src/config.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+use serde::Deserialize;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Key {
+    pub id: String,
+    pub pem: String,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Config {
+    pub key: Vec<Key>,
+}
+
+impl Config {
+    pub fn from_file(path: &str) -> Self {
+        let cfg = fs::read_to_string(path).expect("Unable to read file");
+        let config: Self = toml::from_str(&cfg).expect("Invalid config");
+
+        config
+    }
+}
---
+++ ap-cavage/src/main.rs
@@ -0,0 +1,62 @@
+use sigh::alg::RsaSha256;
+use sigh::{Key, PrivateKey, SigningConfig};
+use url::Url;
+
+use clap::Parser;
+
+mod config;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    config: String,
+
+    #[arg(short, long)]
+    key_id: String,
+
+    #[arg(long)]
+    url: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    let cfg = config::Config::from_file(&args.config);
+    let key = cfg.key.iter().find(|k| k.id == args.key_id).unwrap();
+
+    let private_key = PrivateKey::from_pem(key.pem.as_bytes()).unwrap();
+    // Mastodon only supports rsa-sha256
+    let signing_config = SigningConfig::new(RsaSha256, &private_key, &key.id, false);
+
+    let url: Url = Url::parse(&args.url).unwrap();
+    let host = url.host_str().unwrap();
+
+    let client = reqwest::Client::new();
+    let mut req: http::Request<_> = client
+        .get(url.clone())
+        .header("Accept", "application/activity+json")
+        .header(
+            "Date",
+            chrono::Utc::now()
+                .format("%a, %d %b %Y %H:%M:%S GMT")
+                .to_string(),
+        )
+        .header("Host", host)
+        .build()
+        .unwrap()
+        .try_into()
+        .unwrap();
+
+    println!("req: {:#?}", req);
+
+    signing_config.sign(&mut req).unwrap();
+
+    println!("req: {:#?}", req);
+
+    let result = client.execute(req.try_into().unwrap()).await.unwrap();
+
+    println!("result: {}", result.text().await.unwrap());
+}
